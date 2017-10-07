331b5093c57cb9b7c937ab283a3dd0e97aa37c01
Claire (Xintong) <xintongc@gmail.com>
2017-10-07 17:33:08 -0400

    Create Run.java

---
+++ Run.java
@@ -0,0 +1,17 @@
+package com.zz.web310;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zncu on 2017-09-23.
+ */
+
+@SpringBootApplication
+
+public class Run {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Run.class, args);  //http://localhost:8080/calculate/add
+    }
+}
